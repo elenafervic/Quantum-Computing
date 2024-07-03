d88f872313e174a233b2b4ef2c0aad4c379e4caa
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-03 13:49:41 +0100

    Corrected errors in original file. Imported cirq and numpy, changed printmd to print and added np. to around()

--- arXiv-2010.02931v2/QMExp1.py
+++ arXiv-2010.02931v2/QMExp1.py
@@ -1,3 +1,6 @@
+import cirq
+import numpy as np
+
 # Get a qubit and a circuit
 qbit = cirq.LineQubit(0)
 circuit = cirq.Circuit()
@@ -8,16 +11,16 @@ circuit.append(cirq.X(qbit))
 # Run a simple simulation that extracts the wavefunction of this state
 sim = cirq.Simulator()
 result = sim.simulate(circuit)
-printmd("\n**Bloch Sphere of the qubit in the final state:**")
+print("\n**Bloch Sphere of the qubit in the final state:**")
 state = cirq.bloch_vector_from_state_vector(result.final_state,0)
-print("x: ", around(state[0], 4), " y: ", around(state[1], 4),
-      " z: ", around(state[2], 4))
+print("x: ", np.around(state[0], 4), " y: ", np.around(state[1], 4),
+      " z: ", np.around(state[2], 4))
 
 # Add a measurement at the end of the circuit: 
 circuit.append(cirq.measure(qbit, key="Final state"))
 
 # Display the circuit:
-printmd("\n**Cirq circuit:**")
+print("\n**Cirq circuit:**")
 print(circuit)
 
 # Invoke the Cirq quantum simulator to execute the circuit:
@@ -27,5 +30,5 @@ simulator = cirq.Simulator()
 result = simulator.run(circuit, repetitions=10)
 
 # Print the results:
-printmd("\n**Results of 10 trials:**")
+print("\n**Results of 10 trials:**")
 print(result)
