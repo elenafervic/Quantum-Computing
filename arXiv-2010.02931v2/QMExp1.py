5dcacf73928d3f08a1f7b049d38b6ac91cffbb45
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-03 15:16:52 +0100

    I changed printmd to print because it looked weird on vs code

--- arXiv-2010.02931v2/QMExp1.py
+++ arXiv-2010.02931v2/QMExp1.py
@@ -37,7 +37,8 @@ circuit.append(cirq.X(qbit))
 # Run a simple simulation that extracts the wavefunction of this state
 sim = cirq.Simulator()
 result = sim.simulate(circuit)
-printmd("\n**Bloch Sphere of the qubit in the final state:**")
+#printmd("\n**Bloch Sphere of the qubit in the final state:**")# This was the original spelling, but it looked weird on vs code.
+print("\n**Bloch Sphere of the qubit in the final state:**")
 state = cirq.bloch_vector_from_state_vector(result.final_state_vector,0)
 print("x: ", around(state[0], 4), " y: ", around(state[1], 4),
       " z: ", around(state[2], 4))
@@ -46,7 +47,7 @@ print("x: ", around(state[0], 4), " y: ", around(state[1], 4),
 circuit.append(cirq.measure(qbit, key="Final state"))
 
 # Display the circuit:
-printmd("\n**Cirq circuit:**")
+#printmd("\n**Cirq circuit:**")# This was the original spelling, but it looked weird on vs code.
 print(circuit)
 
 # Invoke the Cirq quantum simulator to execute the circuit:
@@ -56,5 +57,7 @@ simulator = cirq.Simulator()
 result = simulator.run(circuit, repetitions=10)
 
 # Print the results:
-printmd("\n**Results of 10 trials:**")
+
+#printmd("\n**Results of 10 trials:**")# This was the original spelling, but it looked weird on vs code.
+print("\n**Results of 10 trials:**")
 print(result)
